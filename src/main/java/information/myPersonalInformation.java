7b805e627a8ac146ebba0bb906609f40edb15a1c
Aji-Sangha <fnu.ajinchianoohsangha@csuglobal.eedu>
2023-09-23 20:06:50 +0300

    Java Application Program

---
+++ src/main/java/information/myPersonalInformation.java
@@ -0,0 +1,17 @@
+package information;
+
+public class myPersonalInformation {
+    public static void main(String[] args) {
+        String firstName = "Aji";
+        String lastName = "Sangha";
+        String streetAddress = "409 6th Street North Texas City, TX 77590, USA";
+        String city = "Texas";
+        String zipCode = "77590";
+
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Street Address: " + streetAddress);
+        System.out.println("City: " + city);
+        System.out.println("Zip Code: " + zipCode);
+    }
+}
